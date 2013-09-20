d36a83f94a1d5abe4522840d1454a8ce50ac190c
Kevin Thompson <kevin.david.thompson@gmail.com>
2013-09-20 15:00:02 +0000

    adds the notification date to the plus section.

--- app/logic.py
+++ app/logic.py
@@ -18,12 +18,29 @@ def short_stolen_laptop(i):
                   'master_id' : incident_guid },
        'security_incident' : 'Confirmed',
        'source_id' : 'vcdb' }
+  # Fill in some of the root values for the incident. Only include if there is a value present
   for key in ['summary','security_incident','notes','discovery_method']:
     if i[key] != '':
       o[key] = i[key]
+  
+  o['timeline'] = get_incident_timeline(i)
+  o['plus']['timeline'] = get_notification_date(i)
       
   return make_pretty(o)
-                                           
+
+def get_incident_timeline(i):
+  o = {'incident' : {'year' : int(i['compromise_year']) }}
+  if i['compromise_month'] != '' : o['incident']['month'] = int(i['compromise_month'])
+  if i['compromise_day'] != '' : o['incident']['day'] = int(i['compromise_day'])
+  return o
+
+def get_notification_date(i):
+  o = {'year': int(i['disclosure_year'])}
+  if i['discovery_month'] != '': o['month'] = int(i['discovery_month'])
+  if i['discovery_day'] != '': o['year'] = int(i['discovery_year'])
+  return o
+
+  
 def make_pretty(i):
   o = json.dumps(i,indent=2, sort_keys=True, separators=(',', ': '))
   o = o.replace(' ', '&nbsp;')
